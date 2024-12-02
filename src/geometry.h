2f8e842feb8d3128bcfc7c843ef10ac2bf462a52
Lily Nikitin <53998902+boringlily@users.noreply.github.com>
2024-12-02 13:45:13 -0500

    the interface is coming together

--- src/geometry.h
+++ src/geometry.h
@@ -7,6 +7,8 @@ using position = float;
 using distance = float;
 using degree = float;
 using PointIndex = uint32_t;
+using LineIndex = uint32_t;
+using ArcIndex = uint32_t;
 
 struct point2d
 {
@@ -17,23 +19,6 @@ struct point2d
 struct point3d : point2d
 {
     position z{0};
-};
-
-struct Curve{};
-
-struct Line2d : public Curve
-{
-    Line2d(point2d start, distance length, degree angle):start{start},
-    length{length},
-    angle{angle}
-    {};
-
-    Line2d(point2d start, point2d end):start{start},end{end}{};
-
-point2d start{0};
-point2d end{0};
-distance length{0};
-degree angle{0};
 };
 
 namespace CustomMath
@@ -48,6 +33,30 @@ namespace CustomMath
     }
 }
 
+// A Sketch represents a 2D drawing made up of lines and arcs which are 
+// parametrically related via Geometric and Dimensional constraints.
+// All sketches exist on a cartesian XY-plane and use a 3DPlane to position the 2D Sketch in 3D space.
+class Sketch
+{
+public:
+
+enum element_type
+{
+    NONE,
+    POINT,
+    LINE,
+    ARC
+};
+
+// A special type for externally referring to elements of the sketch
+struct GeometryID
+{
+    element_type type: 2;
+    uint32_t index: 30;
+};
+
+static_assert(sizeof(GeometryID)==4);
+
 struct SketchPoint
 {
     position x;
@@ -60,27 +69,48 @@ struct SketchLine
     PointIndex end;
 };
 
-struct SketchCurve
+struct SketchArc
 {
     PointIndex start;
     PointIndex end;
     PointIndex center;
 };
 
+    enum class GeometricConstraints : uint8_t
+    {
+        Horizontal, // Line vector can only point in ±X 
+        Vertical, // Line vector can only point in ±Y 
+        Perpendicular, // L1 is 90 deg from L2
+        Midpoint, // Point snaps to middle of line.
+        Parallel, // Lines can never touch
+        Collinear, // Lines exist on same axis
+        Symmetric, // Elements are identicall and mirrored across axis.
+        Coincident, // P1 and P2 are snapped together.
+        Concentric, // Arc1 and Arc2 have same center.
+        Equal, // All dimentions are shared.
+        Fixed // Position and Size can't change.
+    };
 
-// A sketch object represents a 2D drawing made up of simple lines and arcs. All sketches exist
-// on a cartesian XY-plane. A sketch is created on a 3d plane, which is used for translating from 2d to 3d space.
-class Sketch
-{
-public:
+    enum class DimensionConstraints : uint8_t
+    {
+        LineLength,
+        LineAngle,
+        ArcRadius,
+        ArcAngle
+    };
 
-distance lineLength();
+    using Selection = std::array<GeometryID, 2>;
 
+    bool addLine(point2d start, point2d end);
+    bool addArc(point2d start, point2d center, point2d end);
+    
+    bool addDimension(DimensionConstraints type, distance expression, Selection select);
+    bool addConstraint(GeometricConstraints type, Selection select);
 
 private:
 std::vector<SketchPoint> points; 
 std::vector<SketchLine> lines; 
-std::vector<SketchCurve> curves; 
+std::vector<SketchArc> arcs; 
 
 
 };
