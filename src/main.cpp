f456e2db472414967a7345f860597887286f2247
Lily Nikitin <53998902+boringlily@users.noreply.github.com>
2025-01-02 15:47:33 -0500

    custom 3dnav

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "raylib.h"
 #define RAYGUI_IMPLEMENTATION
+// #define RCAMERA_IMPLEMENTATION
 #include "raygui.h"
 #include "raymath.h"
 #include "rlgl.h"
+#include "rcamera.h"
 
 #include <array>
 #include <vector>
@@ -18,21 +20,10 @@ float grid_spacing_mm = 5;
 constexpr distance GRID_SPACING_MIN{0.001};
 constexpr distance GRID_SPACING_MAX{1000};
 
-namespace UI_2D
-{
-    static float unit_size_scaled{2};
-    
-    void calc_unit_sizes()
-    {
-        unit_size_scaled = 2 * canvas_scale;
-    }
-
-};
-
 struct Line
 {
 public:
-    Vector2 A,B;
+    Vector3 A,B;
 
     void draw(bool simple_colors = true)
     {
@@ -40,9 +31,9 @@ public:
         Color point2 = simple_colors? GRAY : ORANGE;
         Color line =  simple_colors? BLACK : GREEN;
 
-        DrawCircleV(A, 2 * canvas_scale, point1);
-        DrawCircleV(B, 2 * canvas_scale, point2);
-        DrawLine3D(A, B, 2 * canvas_scale, line);
+        // DrawCircleV(A, 2 * canvas_scale, point1);
+        // DrawCircleV(B, 2 * canvas_scale, point2);
+        DrawLine3D(A, B, line);
     }
 
     void draw_with_stats()
@@ -55,28 +46,28 @@ public:
         // DrawText(std::format("{:.2f}mm", length).c_str(), B.x, B.y - 20, 10, BLACK);
 
         // Line Dimension Marker
-        auto PointMarker = [](Vector2 v, degree theta)
-        {
-        //    Vector2 v1 = v + Vector2{1, 1};
-        //    Vector2 v2 = {20, 0};
-        //    v2 = Vector2Rotate(v2, 180 + theta);
-        // theta += 90;
+        // auto PointMarker = [](Vector2 v, degree theta)
+        // {
+        // //    Vector2 v1 = v + Vector2{1, 1};
+        // //    Vector2 v2 = {20, 0};
+        // //    v2 = Vector2Rotate(v2, 180 + theta);
+        // // theta += 90;
             
-            Vector2 v2 = {20 * cos(theta), 10 * sin(theta)};
+        //     Vector2 v2 = {20 * cos(theta), 10 * sin(theta)};
             
-            // DrawText(std::format("{:.2f}deg", theta, v2.x, v2.y).c_str(), v.x, v.y - 20, 10, MAROON);
-            DrawLineEx(v, (v2 + v), 2 * canvas_scale, LIGHTGRAY);
-        };
+        //     // DrawText(std::format("{:.2f}deg", theta, v2.x, v2.y).c_str(), v.x, v.y - 20, 10, MAROON);
+        //     DrawLineEx(v, (v2 + v), 2 * canvas_scale, LIGHTGRAY);
+        // };
 
-        PointMarker(A, angle);
+        // PointMarker(A, angle);
 
         // Draw Angle ARC
-        float ID = length/2;
-        float half_angle = angle * .48; 
+        // float ID = length/2;
+        // float half_angle = angle * .48; 
         
 
         // DrawRing(A,ID, ID + (2 * canvas_scale), 0, half_angle, 20, MAROON);
-        DrawRing(A,ID, ID + (2 * canvas_scale),-half_angle,-angle, 20, DARKBLUE);
+        // DrawRing(A,ID, ID + (2 * canvas_scale),-half_angle,-angle, 20, DARKBLUE);
         // DrawTxt(std::format("{:.2f}deg", angle).c_str(), A.x + length/10, A.y - 20, 10, MAROON);
         
         draw(false);
@@ -84,13 +75,12 @@ public:
 
     void CalcLength()
     {
-        length = Vector2Distance(A,B);
+        // length = Vector3Distance(A,B);
     }
 
     void CalcAngle()
     {
-        // angle = Vector2LineAngle(A, B);
-        angle = Vector2LineAngle(A, B) * CustomMath::radians_to_deg;
+        // angle = Vector3Angle((B-A), {0,10,0}) * CustomMath::radians_to_deg;
         // if(angle < 0)
         // {
         //     angle += 360;
@@ -120,7 +110,7 @@ int main(void)
     InitWindow(screenWidth, screenHeight, "Testing UI");
 
     SetTargetFPS(60);              // Set our game to run at 60 frames-per-second
-    SetExitKey(KEY_F1);
+    SetExitKey(0);
 
     system_font = GetFontDefault();
 
@@ -131,7 +121,7 @@ int main(void)
     Line l2 = {{l1.B.x + 20, l1.B.y} , {500, 380}};
     Line l3 = {{l2.B.x + 20, l2.B.y}, {700, 200}};
 
-    Line follower = {get_canvas_center(), {0,0}};
+    // Line follower = {get_canvas_center(), {0,0}};
 
     Camera2D camera_2d{};
     camera_2d.target = get_canvas_center();
@@ -142,71 +132,32 @@ int main(void)
     // camera_3d.target = {0,0,0};
     // camera_3d.position = {0, 0, 10};
     Camera3D camera_3d = { 0 };
+    camera_3d.up = {.5,0,0};
     camera_3d.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // camera_3d position
     camera_3d.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // camera_3d looking at point
     camera_3d.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // camera_3d up vector (rotation towards target)
     camera_3d.fovy = 45.0f;                                // camera_3d field-of-view Y
     camera_3d.projection = CAMERA_PERSPECTIVE;  
 
-    RenderTexture canvas_texture = LoadRenderTexture(screenWidth, screenHeight);
+    Rectangle toolbar_ui  = {0, 0, screenWidth, 50}; 
+    RenderTexture canvas_texture = LoadRenderTexture(screenWidth, screenHeight-toolbar_ui.height);
     Rectangle canvas_position = {0, 0, (float)canvas_texture.texture.width, (float)-canvas_texture.texture.height};
 
-    DisableCursor();
-    
     while (!WindowShouldClose())
     {   
-        UpdateCamera(&camera_3d, CAMERA_FREE);
-        // if (GuiButton({0, 0, 100, 40}, camera_mode_3d?"Go 2D":"Go 3D")) { camera_mode_3d= !camera_mode_3d;}
+        // UpdateCamera(&camera_3d, CAMERA_THIRD_PERSON);
 
-        // DrawText(TextFormat("Zoom: %0.2f, Scale: %0.2f, Target(%0.2f, %0.2f), Offset(%0.2f, %0.2f)", camera_2d.zoom, canvas_scale, camera_2d.target.x, camera_2d.target.y, camera_2d.offset.x, camera_2d.offset.y), 10, screenHeight - 20, 10, DARKGRAY);
-        // if(IsKeyDown(KEY_LEFT_SHIFT))
-        // {
-        //     DisableCursor();
-        // }
-        // else
-        // {
-        //     EnableCursor();
-        // }
+        CameraMoveToTarget(&camera_3d, -GetMouseWheelMove());
 
-        // if (IsMouseButtonDown(MOUSE_BUTTON_MIDDLE))
-        // {
-        //     Vector2 delta = GetMouseDelta();
-        //     delta = Vector2Scale(delta, -1.0f/camera_2d.zoom);
-        //     camera_2d.target = Vector2Add(camera_2d.target, delta);
-        // }
-        
-        // Zoom based on mouse wheel
-        // float wheel = GetMouseWheelMove();
-        // if (wheel != 0 && IsKeyDown(KEY_LEFT_SHIFT))
-        // {
-        //     if(camera_mode_3d)
-        //     {
-                
-                
-        //     }
-        //     else
-        //     {
-        //         // Get the world point that is under the mouse
-        //         Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera_2d);
-        //         camera_2d.offset = GetMousePosition();
-        //         camera_2d.target = mouseWorldPos;
 
-        //         float scaleFactor = 1.0f + (0.25f*fabsf(wheel));
-        //         if (wheel < 0) scaleFactor = 1.0f/scaleFactor;
-        //         camera_2d.zoom = Clamp(camera_2d.zoom*scaleFactor, 0.125f, 64.0f);
-        //         canvas_scale = 1/camera_2d.zoom;
-        //     }
-        // }
-
-        // canvas_scale = int(camera_2d.zoom);
         Vector3 mouse_world_pos = GetScreenToWorldRay(GetMousePosition(), camera_3d).position;
         mouse_pos = GetMousePosition();
 
-        
-        follower.B = Vector2({mouse_world_pos.x, mouse_world_pos.y});
-
         BeginTextureMode(canvas_texture);
             ClearBackground(RAYWHITE);
+            
+                // DrawText(std::format("Target({}), Position({})", camera_3d.target.y, camera_3d.target.x, camera_3d.target.z, camera_3d.position).c_str(), 10, screenHeight - 20, 10, DARKGRAY);
+                DrawText(TextFormat("UP(%0.2f, %0.2f, %0.2f), Position(%0.2f, %0.2f, %0.2f)", camera_3d.up.y, camera_3d.up.x, camera_3d.up.z, camera_3d.position.y, camera_3d.position.x, camera_3d.position.z), 10, canvas_texture.texture.height - 20, 10, DARKGRAY);
 
                 // BeginMode3D(camera_3d);
                 //     // rlPushMatrix();
@@ -216,34 +167,27 @@ int main(void)
                     
                 //     DrawGrid(100, 50);
                     
-                    follower.draw_with_stats();
                 // EndMode3D();
 
                 BeginMode3D(camera_3d);
 
-                DrawCube({0,0,0}, 2.0f, 2.0f, 2.0f, RED);
-                DrawCubeWires({0,0,0}, 2.0f, 2.0f, 2.0f, MAROON);
+                    DrawCube({0,0,0}, 2.0f, 2.0f, 2.0f, RED);
+                    DrawCubeWires({0,0,0}, 2.0f, 2.0f, 2.0f, MAROON);
 
-                DrawGrid(10, 1.0f);
+                    // rlPushMatrix();
+                    // rlTranslatef(0, 25*50, 0);
+                    // rlRotatef(90, 0, 1, 0);
+                    // rlPopMatrix();
+                    // DrawGrid(10, 1.0f);
 
             EndMode3D();
-
-            follower.draw_with_stats();
-
-            DrawRectangle( 10, 10, 320, 93, Fade(SKYBLUE, 0.5f));
-            DrawRectangleLines( 10, 10, 320, 93, BLUE);
-
-            DrawText("Free camera default controls:", 20, 20, 10, BLACK);
-            DrawText("- Mouse Wheel to Zoom in-out", 40, 40, 10, DARKGRAY);
-            DrawText("- Mouse Wheel Pressed to Pan", 40, 60, 10, DARKGRAY);
-            DrawText("- Z to zoom to (0, 0, 0)", 40, 80, 10, DARKGRAY);
 
         EndTextureMode();
 
 
        BeginDrawing();
         ClearBackground(SKYBLUE);
-            DrawTextureRec(canvas_texture.texture, canvas_position, {0,0}, WHITE);
+            DrawTextureRec(canvas_texture.texture, canvas_position, {0, toolbar_ui.height}, WHITE);
         EndDrawing();
     }
 
