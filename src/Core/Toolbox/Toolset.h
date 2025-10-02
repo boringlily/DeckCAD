356e452618a96b2d7f1781cbceccf8a0f96f7862
Lily Nikitin <53998902+boringlily@users.noreply.github.com>
2025-10-02 09:43:52 -0700

    Implementation of a very simple UI with header tabs and toolbox

---
+++ src/Core/Toolbox/Toolset.h
@@ -0,0 +1,97 @@
+using DrawToolsetFunc = void (*)();
+
+inline bool MouseClickedAndHovered()
+{
+    return Clay_Hovered() && IsMouseButtonReleased(MOUSE_BUTTON_LEFT);
+}
+
+Clay_ElementDeclaration ToolboxButtonConfig(bool active)
+{
+    return {
+        .layout = {
+            .sizing = { .width = CLAY_SIZING_GROW(), .height = CLAY_SIZING_FIT() },
+            .padding = CLAY_PADDING_ALL(4),
+            .childAlignment = ALIGN_CENTER,
+        },
+        .backgroundColor = active || Clay_Hovered() ? GuiTheme.BgBase : GuiTheme.BgLight,
+        .cornerRadius = { 4u },
+    };
+}
+
+static Toolbox::ToolStatus PlaceholderFunc()
+{
+    auto status = Toolbox::ToolStatus::active;
+
+    CLAY({ .layout = {
+               .sizing = { .width = CLAY_SIZING_GROW(), .height = CLAY_SIZING_FIT() },
+               .padding = CLAY_PADDING_ALL(4),
+               .childGap = 8,
+               .layoutDirection = CLAY_TOP_TO_BOTTOM,
+           },
+        .backgroundColor = GuiTheme.BgBase })
+    {
+        CLAY_TEXT(CLAY_STRING("Hi, you have clicked a placeholder. This tool currently is in-development and will be enabled once it is ready"), &TextStyle.body);
+        CLAY(ToolboxButtonConfig(false))
+        {
+            CLAY_TEXT(CLAY_STRING("Ok, I understand."), &TextStyle.buttonActive);
+
+            if (MouseClickedAndHovered()) {
+                status = Toolbox::ToolStatus::done;
+            }
+        };
+    }
+
+    return status;
+}
+
+constexpr void BeginToolGroup(Clay_String group_name)
+{
+    Clay_ElementDeclaration group = {
+        .layout = {
+            .sizing = { .width = CLAY_SIZING_GROW(), .height = CLAY_SIZING_FIT() },
+            .padding = CLAY_PADDING_ALL(8),
+            .childGap = 8,
+            .layoutDirection = CLAY_TOP_TO_BOTTOM,
+        },
+        .cornerRadius = CLAY_CORNER_RADIUS(10),
+        .border = (Clay_BorderElementConfig) { .color = GuiTheme.BorderBase, .width = { 2, 2, 2, 2, 0 } }
+    };
+
+    Clay__OpenElement();
+    Clay__ConfigureOpenElement(group);
+
+    CLAY_TEXT(group_name, &TextStyle.subtitle);
+}
+
+constexpr void EndToolGroup()
+{
+    Clay__CloseElement();
+}
+
+constexpr void ToolSelectButton(std::string_view name, IconId icon, Toolbox::ToolFunctionPointer function)
+{
+    CLAY({
+        .layout = {
+            .sizing = { .width = CLAY_SIZING_GROW(), .height = CLAY_SIZING_FIT() },
+            .padding = CLAY_PADDING_ALL(4),
+            .childGap = 4,
+            .childAlignment = { .x = CLAY_ALIGN_X_LEFT, .y = CLAY_ALIGN_Y_CENTER },
+            .layoutDirection = CLAY_LEFT_TO_RIGHT,
+        },
+        .backgroundColor = Clay_Hovered() ? GuiTheme.BgLight : GuiTheme.BgBase,
+        .cornerRadius = CLAY_CORNER_RADIUS(8),
+    })
+    {
+
+        DrawIcon(icon, GuiTheme.TextBase);
+
+        static Clay_String tool_name {};
+        tool_name = { true, static_cast<s32>(name.size()), name.data() };
+
+        CLAY_TEXT(tool_name, &TextStyle.body);
+
+        if (Clay_Hovered() && IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+            app_global->GetCurrentScene().toolbox.active_tool = function;
+        }
+    };
+}
--- src/Core/Workbench.cpp
+++ src/Core/Workbench.cpp
@@ -3,8 +3,15 @@
 #include "Explorer/Explorer.cpp"
 #include "Canvas/Canvas.cpp"
 #include "Toolbox/Toolbox.cpp"
+<<<<<<< HEAD
 
 inline void DrawWorkbench(Scene& scene)
+    == == ==
+    =
+#include "AppMemory.h"
+
+        inline void DrawWorkbench(AppMemory & app)
+>>>>>>> ImplementingBasicUi
 {
     /// Workbench components
     CLAY({ .id = CLAY_ID("Workbench"),
@@ -19,11 +26,18 @@ inline void DrawWorkbench(Scene& scene)
                 .layout = { .sizing = LAYOUT_EXPAND,
                     .layoutDirection = CLAY_LEFT_TO_RIGHT } })
         {
+<<<<<<< HEAD
             DrawExplorer(scene);
 
             DrawCanvas(scene);
 
             DrawToolbox(scene);
+            == == == = DrawExplorer(app.GetCurrentScene());
+
+            DrawCanvas(app.GetCurrentScene());
+
+            DrawToolbox(app.GetCurrentScene());
+>>>>>>> ImplementingBasicUi
         };
 
         CLAY(
@@ -34,9 +48,15 @@ inline void DrawWorkbench(Scene& scene)
                     .childAlignment = { .y = CLAY_ALIGN_Y_CENTER },
                     .layoutDirection = CLAY_LEFT_TO_RIGHT,
                 },
+<<<<<<< HEAD
                 .backgroundColor = COLOR_LIGHT_GREY })
         {
             DrawIconElement(IconId::Settings, GuiTheme.TextBase);
+            == == == =
+                .backgroundColor = GuiTheme.BgDark
+        })
+        {
+>>>>>>> ImplementingBasicUi
             CLAY_TEXT(CLAY_STRING("Active Mode: {UNKNOWN}"), &TextStyle.body);
         };
     };
